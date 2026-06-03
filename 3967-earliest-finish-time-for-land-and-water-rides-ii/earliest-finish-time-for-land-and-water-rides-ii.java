af49acef36a179e8180707a555a8f39364cc315d
Ajay Chaudhari <148574129+ajay26311@users.noreply.github.com>
2026-06-03 23:31:06 +0530

    Time: 201 ms (10.35%) | Memory: 102.7 MB (5.17%) - LeetSync

---
+++ 3967-earliest-finish-time-for-land-and-water-rides-ii/earliest-finish-time-for-land-and-water-rides-ii.java
@@ -0,0 +1,99 @@
+class Solution {
+    public int earliestFinishTime(int[] landStartTime, int[] landDuration,
+                                  int[] waterStartTime, int[] waterDuration) {
+
+        long ans = Long.MAX_VALUE;
+
+        ans = Math.min(ans,
+                solve(landStartTime, landDuration,
+                      waterStartTime, waterDuration));
+
+        ans = Math.min(ans,
+                solve(waterStartTime, waterDuration,
+                      landStartTime, landDuration));
+
+        return (int) ans;
+    }
+
+    private long solve(int[] firstStart, int[] firstDur,
+                       int[] secondStart, int[] secondDur) {
+
+        int m = secondStart.length;
+
+        int[][] rides = new int[m][2];
+        for (int i = 0; i < m; i++) {
+            rides[i][0] = secondStart[i];
+            rides[i][1] = secondDur[i];
+        }
+
+        Arrays.sort(rides, (a, b) -> Integer.compare(a[0], b[0]));
+
+        int[] starts = new int[m];
+
+        long[] prefixMinDur = new long[m];
+        long[] suffixMinOpenPlusDur = new long[m];
+
+        for (int i = 0; i < m; i++) {
+            starts[i] = rides[i][0];
+        }
+
+        prefixMinDur[0] = rides[0][1];
+        for (int i = 1; i < m; i++) {
+            prefixMinDur[i] = Math.min(prefixMinDur[i - 1], rides[i][1]);
+        }
+
+        suffixMinOpenPlusDur[m - 1] =
+                (long) rides[m - 1][0] + rides[m - 1][1];
+
+        for (int i = m - 2; i >= 0; i--) {
+            suffixMinOpenPlusDur[i] =
+                    Math.min(
+                            suffixMinOpenPlusDur[i + 1],
+                            (long) rides[i][0] + rides[i][1]
+                    );
+        }
+
+        long ans = Long.MAX_VALUE;
+
+        for (int i = 0; i < firstStart.length; i++) {
+
+            long endFirst = (long) firstStart[i] + firstDur[i];
+
+            int idx = upperBound(starts, (int) endFirst);
+
+            // waterStart <= endFirst
+            if (idx > 0) {
+                ans = Math.min(
+                        ans,
+                        endFirst + prefixMinDur[idx - 1]
+                );
+            }
+
+            // waterStart > endFirst
+            if (idx < m) {
+                ans = Math.min(
+                        ans,
+                        suffixMinOpenPlusDur[idx]
+                );
+            }
+        }
+
+        return ans;
+    }
+
+    private int upperBound(int[] arr, int target) {
+        int l = 0, r = arr.length;
+
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+
+            if (arr[mid] <= target) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+
+        return l;
+    }
+}
