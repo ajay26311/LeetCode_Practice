70af6179c243be34721b7c36b9280e0c8d03a489
Ajay Chaudhari <148574129+ajay26311@users.noreply.github.com>
2026-07-07 22:56:17 +0530

    Time: 14 ms (71.05%) | Memory: 45.1 MB (48.27%) - LeetSync

--- 837-most-common-word/most-common-word.java
+++ 837-most-common-word/most-common-word.java
@@ -1,36 +1,31 @@
 class Solution {
     public String mostCommonWord(String paragraph, String[] banned) {
-        
-        paragraph = paragraph.toLowerCase().replaceAll("[^a-z]", " ");
-
+        paragraph = paragraph.toLowerCase();
+        paragraph = paragraph.replaceAll("[!?',;.]", " ");
         String[] words = paragraph.split("\\s+");
-
+        HashSet<String> ban = new HashSet<>();
+        for (String b : banned) 
+        {
+            ban.add(b);
+        }
         HashMap<String, Integer> map = new HashMap<>();
-
-        String ans = "";
-        int max = 0;
-
-        for (String word : words) {
-
-            boolean isBanned = false;
-
-            for (String b : banned) {
-                if (word.equals(b)) {
-                    isBanned = true;
-                    break;
-                }
-            }
-
-            if (!isBanned) {
+        for (String word : words) 
+        {
+            if (!ban.contains(word)) 
+            {
                 map.put(word, map.getOrDefault(word, 0) + 1);
-
-                if (map.get(word) > max) {
-                    max = map.get(word);
-                    ans = word;
-                }
             }
         }
-
+        String ans = "";
+        int max = 0;
+        for (Map.Entry<String, Integer> entry : map.entrySet()) 
+        {
+            if (entry.getValue() > max) 
+            {
+                max = entry.getValue();
+                ans = entry.getKey();
+            }
+        }
         return ans;
     }
 }
