3c9e59c73226b85e93c32220fa827a057e57fa29
Ajay Chaudhari <148574129+ajay26311@users.noreply.github.com>
2026-08-07 02:51:57 -0700

    Time: 1 ms (99.74%) | Memory: 46.5 MB (92.53%) - LeetSync

--- 3778-transform-array-by-parity/transform-array-by-parity.java
+++ 3778-transform-array-by-parity/transform-array-by-parity.java
@@ -12,7 +12,19 @@ class Solution {
                 nums[i] = 1;
             }
         }
-        Arrays.sort(nums);
+        
+        for(int i = 0; i < nums.length; i++)
+        {
+            for(int j = i+1; j< nums.length; j++)
+            {
+                if(nums[i] > nums[j])
+                {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
         return nums;
     }
 }
