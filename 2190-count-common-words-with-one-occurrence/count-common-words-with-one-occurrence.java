554284187ed6f0a1d351f7bc9a1de272301bce68
Ajay Chaudhari <148574129+ajay26311@users.noreply.github.com>
2026-08-28 05:12:39 -0700

    Time: 7 ms (72.57%) | Memory: 46.4 MB (45.13%) - LeetSync

---
+++ 2190-count-common-words-with-one-occurrence/count-common-words-with-one-occurrence.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int countWords(String[] words1, String[] words2) {
+        HashMap<String, Integer> map1 = new HashMap<>();
+        HashMap<String, Integer> map2 = new HashMap<>();
+
+        // Count frequency of words1
+        for (String word : words1) {
+            map1.put(word, map1.getOrDefault(word, 0) + 1);
+        }
+
+        // Count frequency of words2
+        for (String word : words2) {
+            map2.put(word, map2.getOrDefault(word, 0) + 1);
+        }
+
+        int count = 0;
+
+        // Check words appearing exactly once in both
+        for (String word : map1.keySet()) {
+            if (map1.get(word) == 1 && map2.getOrDefault(word, 0) == 1) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
